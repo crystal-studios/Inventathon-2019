70c128a50ac7795e420747838c301d07fb7035bd
KingWaffleIII <nonalio@zoho.eu>
2019-08-06 13:17:35 +0000

    Update Dialogue.py

--- Dialogue.py
+++ Dialogue.py
@@ -5,7 +5,7 @@ def start():
   print("You have crash-landed on a distant alien planet.")
   sleep(1)
   print("Your shuttle has recieved varying degrees of damage, the most severe areas being:")
-  sleep(2)
+  sleep(1)
   print(".The thrusters")
   sleep(0.5)
   print(".The engine")
@@ -14,12 +14,13 @@ def start():
   sleep(1)
   print("To leave this planet and reach the Moon, you must repair the broken pieces of the shuttle.")
   sleep(1)
-  print("Pieces you will need to repair are found all over this planet.NOW GET LOST!!!!!")
+  print('''Pieces you will need to repair are found all over this planet. To find out what these items are,
+  use the "examine" command followed by the part which you wnat to repair.''')
+  loop()
   
-  
-  
-  #the end bit sequence thing starts here cuz why not
-  sleep(4)#after the repair thing has been activated
+def leave():
+  #the end bit sequence thing starts here
+  sleep(4)
   print("The shuttle has been successfully repaired and you can leave the alien planet.")
   sleep(1)
-  print("Unfortunately, the thrusters have been  too powerful and have yeeted you and your shuttle into the moon.")
+  print("You board ")
