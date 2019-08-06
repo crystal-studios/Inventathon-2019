f171bb15d2e175306e7bbbd38fddc8aab3ecf04f
KingWaffleIII <nonalio@zoho.eu>
2019-08-06 12:27:52 +0000

    Update Dialogue.py

---
+++ Dialogue.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+def start():
+
+  print("You have crash-landed on a distant alien planet.")
+  sleep(1)
+  print("Your shuttle has recieved varying degrees of damage, the most severe areas being:")
+  sleep(2)
+  print(".The thrusters")
+  sleep(0.5)
+  print(".The engine")
+  sleep(0.5)
+  print(".and the hull")
+  sleep(1)
+  print("To leave this planet and reach the Moon, you must repair the broken pieces of the shuttle.")
+  sleep(1)
+  print("Pieces you will need to repair are found all over this planet.NOW GET LOST!!!!!")
+  
+  
+  
+  #the end bit sequence thing starts here cuz why not
+  sleep(4)#after the repair thing has been activated
+  
